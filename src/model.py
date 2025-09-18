784c3f127dd02f8a2070e3c4c67df24070633acf
Sai Charan <saicharan.kokkula27@gmail.com>
2025-09-18 22:54:27 +0530

    Updated model.py, added requirements.txt and .gitignore

--- src/model.py
+++ src/model.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from crewai import LLM
+import streamlit as st
 
 # Load environment variables
 load_dotenv()
@@ -8,7 +9,7 @@ load_dotenv()
 # Gemini LLM setup
 GEMINI_LLM = LLM(
     model="gemini/gemini-1.5-flash",
-    api_key=os.getenv("GEMINI_API_KEY"),
+    api_key=os.st.secrets[("GEMINI_API_KEY")],
     temperature=0.7,
     max_tokens=2048,
 )
